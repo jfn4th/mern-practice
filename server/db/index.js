cc82fa49fcf6d03cd19df522fa9e677bb38a4c60
Jamie Nelson <jfn4th@gmail.com>
2020-07-10 19:40:40 -0500

    add db connection

---
+++ server/db/index.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+// dotenv
+require('dotenv').config();
+
+// mongoDB connection
+mongoose
+    .connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => {
+        console.log('Connection successful!');
+    })
+    .catch((err) => {
+        console.log('ERROR: ', err.message);
+    });
+// mongoose.set('useFindAndModify', false);
+
+const db = mongoose.connection;
+
+module.exports = db;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    db = require('./db');
+
 const app = express(),
     apiPort = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
+
+db.on('error', console.error.bind(console, 'MongoDB Atlas connection error:'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
