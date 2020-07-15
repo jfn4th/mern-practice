a5735b686601c18eee3e01d4082d468c1314395d
Jamie Nelson <jfn4th@gmail.com>
2020-07-15 12:08:11 -0500

    add delete and get movie controllers

--- server/controllers/movie-ctrl.js
+++ server/controllers/movie-ctrl.js
@@ -43,7 +43,7 @@ updateMovie = async (req, res) => {
         });
     }
 
-    Movie.findOne({ _id: req.params.id }, (err, movie) => {
+    await Movie.findOne({ _id: req.params.id }, (err, movie) => {
         if (err) {
             return res.status(404).json({
                 err,
@@ -71,8 +71,49 @@ updateMovie = async (req, res) => {
     });
 };
 
-deleteMovie = async (req, res) => {};
+deleteMovie = async (req, res) => {
+    await Movie.findOneAndDelete({ _id: req.params.id }, (err, movie) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err });
+        }
 
-getMovieById = async (req, res) => {};
+        if (!movie) {
+            return res.status(404).json({ success: false, error: `Movie not found` });
+        }
 
-getMovies = async (req, res) => {};
+        return res.status(200).json({ success: true, data: movie });
+    }).catch((err) => console.log(err));
+};
+
+getMovieById = async (req, res) => {
+    await Movie.findOne({ _id: req.params.id }, (err, movie) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err });
+        }
+
+        if (!movie) {
+            return res.status(404).json({ success: false, error: `Movie not found` });
+        }
+        return res.status(200).json({ success: true, data: movie });
+    }).catch((err) => console.log(err));
+};
+
+getMovies = async (req, res) => {
+    await Movie.find({}, (err, movies) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err });
+        }
+        if (!movies.length) {
+            return res.status(404).json({ success: false, error: `Movie not found` });
+        }
+        return res.status(200).json({ success: true, data: movies });
+    }).catch((err) => console.log(err));
+};
+
+module.exports = {
+    createMovie,
+    updateMovie,
+    deleteMovie,
+    getMovies,
+    getMovieById
+};
