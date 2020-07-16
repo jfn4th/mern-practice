80f27a558596f6ef77e6ad094a0ad79548b6dbba
Jamie Nelson <jfn4th@gmail.com>
2020-07-16 14:48:42 -0500

    add routes

---
+++ client/src/api/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000/api'
+});
+
+export const insertMovie = (payload) => api.post(`/movie`, payload);
+export const getAllMovies = () => api.get(`/movies`);
+export const getMovieById = (id) => api.get(`/movie/${id}`);
+export const updateMovieById = (id, payload) => api.put(`/mavue/${id}`, payload);
+export const deleteMovieById = (id) => api.delete(`/movie/${id}`);
