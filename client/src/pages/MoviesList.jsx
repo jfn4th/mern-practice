201c84190ae1c90f247f62225b701a389fcd7b5b
Jamie Nelson <jfn4th@gmail.com>
2020-07-18 08:21:05 -0500

    make barebones pages

---
+++ client/src/pages/MoviesList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class MoviesList extends Component {
+    render() {
+        return (
+            <div>
+                <p>On this page you'll see the list of movies</p>
+            </div>
+        );
+    }
+}
---
+++ client/src/pages/MoviesUpdate.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class MoviesUpdate extends Component {
+    render() {
+        return (
+            <div>
+                <p>On this page you'll see the form to update the movies</p>
+            </div>
+        );
+    }
+}
